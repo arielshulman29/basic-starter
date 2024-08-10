1b052864c4b61a0ec2633c98f65f443c32a6f5de
arielshulman <ariel@illumex.ai>
2024-08-10 12:39:06 +0300

    setup routes

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,18 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import { getRestaurants } from './restaurants/service';
+
 const app = express();
 const port = process.env.PORT;
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello World!" });
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api', async (req, res) => {
+	const restaurants = await getRestaurants();
+	res.status(200).json({ restaurants });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
